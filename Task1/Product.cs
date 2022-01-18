88a0859ffdd35f9e0339ae030edaf1f3476a8b2b
todua10 <todua10@inbox.ru>
2022-01-18 22:26:35 +0500

    upd

--- Task1/Product.cs
+++ Task1/Product.cs
@@ -6,14 +6,31 @@ using System.Threading.Tasks;
 
 namespace Task1
 {
-    abstract class Product
+    class Product
     {
+        public Dictionary<string, double> prices = new Dictionary<string, double>
+        {
+            ["Кофе"] = 60,
+            ["Сок"] = 75,
+            ["Какао"] = 100,
+            ["Макароны"] = 125,
+            ["Пюре"] = 170,
+            ["Говяжья котлета"] = 1416,
+            ["Куриная котлета"] = 916
+        };
         public string Name { get; set; }
         public double Price { get; set; }
-        public Product(string name)
+        public double Size { get; set; }
+
+        public Product(string name, double size)
         {
             Name = name;
+            Size = size;
+            Price = prices[name];
         }
-        public abstract double GetCost();
+        public double GetCost() 
+        {
+            return Price * Size;
+        }
     }
 }
